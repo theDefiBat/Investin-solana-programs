2cc5e683b47df9e472e198beaf7eba4dccbca81e
mabrehan <mabrehan13@gmail.com>
2021-05-06 13:25:32 +0530

    update

--- src/program-rust/src/Sample/entrypoint.rs
+++ src/program-rust/src/Sample/entrypoint.rs
@@ -15,6 +15,14 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    // let choice = 1; //have to get this from instruction_data
+
+    // match choice {
+    //     1 => crate::deposit::Deposit(program_id, accounts, instruction_data),
+
+    //     2 => crate::withdraw::Withdraw(program_id, accounts, instruction_data)
+    // }
+
     crate::deposit::Deposit(program_id, accounts, instruction_data)
     // Withdraw::process(program_id, accounts, instruction_data)
 }
