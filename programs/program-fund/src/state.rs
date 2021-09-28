4b7cd5a067f50dde1b7ebcfae29d5f7eeaf9d87f
Muneeb Mohammed <mohammedabdulmuneeb98@gmail.com>
2021-09-28 22:43:51 +0530

    add selective transfer to vault

--- programs/program-fund/src/state.rs
+++ programs/program-fund/src/state.rs
@@ -314,6 +314,9 @@ impl FundData {
     pub fn get_margin_index(&self, margin_account_pk: &Pubkey) -> Option<usize> {
         self.mango_positions.iter().position(|pos| pos.margin_account == *margin_account_pk)
     }
+    pub fn get_investor_index(&self, inv_state_pk: &Pubkey) -> Option<usize> {
+        self.investors.iter().position(|pos| *pos == *inv_state_pk)
+    }
 }
 
 impl InvestorData {
