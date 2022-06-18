293f8a6d23bc26670aa0519c43f4098068153702
zoheb-mzs <zohebmzsedu@gmail.com>
2022-06-18 14:23:52 +0400

    Process Deposit

--- frontend/src/components/ProcessDeposits.js
+++ frontend/src/components/ProcessDeposits.js
@@ -1,11 +1,11 @@
 import { PublicKey, Transaction, TransactionInstruction, create} from '@solana/web3.js';
 import React, { useState } from 'react'
 import { GlobalState } from '../store/globalState';
-import { connection, programId, platformStateAccount, FUND_ACCOUNT_KEY, TOKEN_PROGRAM_ID } from '../utils/constants';
+import { connection, programId, platformStateAccount, FUND_ACCOUNT_KEY, TOKEN_PROGRAM_ID, SYSTEM_PROGRAM_ID } from '../utils/constants';
 import { nu64, struct, u32 } from 'buffer-layout';
 import { createKeyIfNotExists, findAssociatedTokenAddress, signAndSendTransaction, createAssociatedTokenAccountIfNotExist, createAccountInstruction } from '../utils/web3';
 import { FUND_DATA, INVESTOR_DATA } from '../utils/programLayouts';
-import { awaitTransactionSignatureConfirmation, IDS, MangoClient } from '@blockworks-foundation/mango-client';
+import { awaitTransactionSignatureConfirmation, IDS, MangoClient, NodeBankLayout } from '@blockworks-foundation/mango-client';
 import { sendSignedTransactionAndNotify } from '../utils/solanaWeb3';
 
 export const ProcessDeposits = () => {
@@ -26,7 +26,10 @@ export const ProcessDeposits = () => {
       return;
     };
 
-    const fundPDA = await PublicKey.findProgramAddress([walletProvider?.publicKey.toBuffer()], programId)[0];
+
+
+    const fundPDA = (await PublicKey.findProgramAddress([walletProvider?.publicKey.toBuffer()], programId))[0];
+    console.log("fundPDA::",fundPDA.toBase58())
 
     console.log('selected investment::', selectedInvestmentStateAcc)
   
@@ -40,27 +43,51 @@ export const ProcessDeposits = () => {
           INVESTOR_DATA.span,
           'singleGossip'
         )
-    let signers = [];
     
-    const investorBaseTokenAccount = await createAssociatedTokenAccountIfNotExist(walletProvider, new PublicKey(ids.tokens[0].mintKey), key, transaction);
 
     console.log("account size::: ", INVESTOR_DATA.span)
+
+    let client = new MangoClient(connection, new PublicKey(ids.mangoProgramId))
+    let mangoGroup = await client.getMangoGroup(new PublicKey(ids.publicKey))
+    let nodeBankInfo = await connection.getAccountInfo(new PublicKey(ids.tokens[0].nodeKeys[0]))
+    let nodeBank = NodeBankLayout.decode(nodeBankInfo.data)
 
     const dataLayout = struct([u32('instruction')])
     const data = Buffer.alloc(dataLayout.span)
     dataLayout.encode(
       {
-        instruction: 2,
+        instruction: 4,
       },
       data
     )
     const keys =  [
       { pubkey: new PublicKey(fundPDA), isSigner: false, isWritable: true }, //fund State Account
-      // { pubkey: investerStateAccount, isSigner: false, isWritable: true },
       { pubkey: key, isSigner: true, isWritable: true },
-      { pubkey: investorBaseTokenAccount, isSigner: false, isWritable: true }, // Investor Base Token Account
+      { pubkey: new PublicKey(ids.mangoProgramId), isSigner: false, isWritable: false },
+      { pubkey: new PublicKey(ids.publicKey), isSigner: false, isWritable: true },
+      { pubkey: fundState.mango_account, isSigner: false, isWritable: true },
+      { pubkey: mangoGroup.mangoCache, isSigner: false, isWritable: true },
+      { pubkey: new PublicKey('AMzanZxMirPCgGcBoH9kw4Jzi9LFMomyUCXbpzDeL2T8'), isSigner: false, isWritable: true }, //root_bank_ai
+      { pubkey: new PublicKey('BGcwkj1WudQwUUjFk78hAjwd1uAm8trh1N4CJSa51euh'), isSigner: false, isWritable: true }, //node_bank_ai
+      { pubkey: nodeBank.vault, isSigner: false, isWritable: true }, //vault_ai
+      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
       { pubkey: fundState.usdc_vault_key, isSigner: false, isWritable: true },
-      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false }
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: new PublicKey(selectedInvestmentStateAcc), isSigner: false, isWritable: true }
     ];
 
     for(let i = 0; i<keys.length; i++){
@@ -80,8 +107,6 @@ export const ProcessDeposits = () => {
     let hash = await connection.getRecentBlockhash();
     console.log("tx", transaction);
     transaction.recentBlockhash = hash.blockhash;
-    transaction.setSigners(key);
-    transaction.partialSign(...signers)
 
     // const sign = await signAndSendTransaction(walletProvider, transaction);
     // console.log("signature tx:: ", sign)
